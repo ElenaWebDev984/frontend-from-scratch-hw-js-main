f9977b3a5f0b9ce1c331317869efdcc3e2c4b902
ElenaWebDev984 <badalova.dev.984@gmail.com>
2024-10-19 22:13:46 +0200

    lesson-02/02

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -9,7 +9,7 @@
 
 let dice1 = Math.floor(Math.random() * 6); // your code
 let dice2 = Math.floor(Math.random() * 6); // your code
-let isWinningDouble = dice1 > 3 && dice2 > 3;// your code
+let isWinningDouble = dice1 === dice2 && dice2 > 3;// your code
 
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
