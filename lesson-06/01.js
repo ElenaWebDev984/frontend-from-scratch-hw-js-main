1c44a860d3faf6c53a84def7aab26b52233b84cd
ElenaWebDev984 <badalova.dev.984@gmail.com>
2024-11-10 18:43:00 +0100

    lesson-06/01

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -17,3 +17,33 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+onst WEB_TECH_IMAGES = [
+  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
+  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
+  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
+];
+
+let currentIndex = 0;  // Tracks the current image index
+
+const imageElement = document.getElementById('web-tech-image');
+
+function updateImage() {
+  imageElement.src = WEB_TECH_IMAGES[currentIndex]; // Update the src of the img tag
+}
+
+function showPrevImage() {
+  currentIndex = (currentIndex - 1 + WEB_TECH_IMAGES.length) % WEB_TECH_IMAGES.length; // Calculate the previous index
+  updateImage(); // Update the image displayed
+}
+
+function showNextImage() {
+  currentIndex = (currentIndex + 1) % WEB_TECH_IMAGES.length; // Calculate the next index
+  updateImage(); // Update the image displayed
+}
+
+document.getElementById('prev-button').addEventListener('click', showPrevImage);
+document.getElementById('next-button').addEventListener('click', showNextImage);
+
+// Initially display the first image
+updateImage();
